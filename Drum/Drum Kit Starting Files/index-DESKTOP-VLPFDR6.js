2eec47cc9c5abfc0272858f84272a2299fe313c4
thihoang1007 <thoang9@huskers.unl.edu>
2023-02-08 10:47:13 -0600

    remove duplicate files

--- Drum/Drum Kit Starting Files/index-DESKTOP-VLPFDR6.js
+++
@@ -1,7 +0,0 @@
-var numberOfButton = document.querySelectorAll(".drum").length;
-for (let i = 0; i < numberOfButton; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function handleClick() {
-        alert("Click");
-    }); 
-        
-}
